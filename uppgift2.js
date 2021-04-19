4a52b625fa878bb7fb71052d4acf9297352e81de
Veronica Wu <maypeppermint@gmail.com>
2021-04-19 17:57:19 +0200

    Uppgift2

---
+++ uppgift2.js
@@ -0,0 +1,7 @@
+/*
+Skapa ett "program" som skriver ut "Hello World" i konsollen 10 gånger med hjälp av en for-loop. 
+Varje rad ska ha iterationens nummer, 1 till 10.
+*/
+for (let i = 1; i < 11; i++) {
+    console.log((i) + ". " + "Hello World");
+}
